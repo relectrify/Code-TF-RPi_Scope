3adecedfe0557b25c56b08a6d59981a5c1046f88
milescaples <48931002+milescaples@users.noreply.github.com>
2019-04-04 13:04:54 +1100

    Update rpi_scope.py
    Refactor write to csv

--- rpi_scope.py
+++ rpi_scope.py
@@ -1,6 +1,11 @@
 import visa
 from rigol import ds1000z
 
+def raw_data_to_string(raw_data):
+    string = str(raw_data).strip("b\'").strip("\\ne")
+    string = string.replace(',', '\n')
+    return string
+
 def main():
     rm = visa.ResourceManager('@py')
     resources = rm.list_resources()
@@ -9,8 +14,12 @@ def main():
     device = rm.open_resource(usb[0])
     device.timeout = None
     scope = ds1000z.Ds1000z(device)
-    print(scope.get_id())
     scope.get_screenshot("test.png")
+
+    waveform = raw_data_to_string(scope.get_data())
+    with open("waveform.csv", 'w') as f:
+        for point in waveform:
+            f.write(point)
 
 if __name__ == "__main__":
     main()
