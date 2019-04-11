e5e8dc6e51f7d35e92d197f3838be7a38409b2c4
milescaples <48931002+milescaples@users.noreply.github.com>
2019-04-11 12:35:24 +1000

    Update rpi_scope.py
    Add 4 buttons to select which channels to export data, testing appending csv with data, pushing to test on actually hardware

--- rpi_scope.py
+++ rpi_scope.py
@@ -10,6 +10,9 @@ import boto3
 import time
 import datetime
 import uuid
+import csv
+from random import randint
+from shutil import copyfile
 
 def raw_data_to_string(raw_data):
     string = str(raw_data).strip("b\'").strip("\\ne")
@@ -30,12 +33,12 @@ class ScopeCapture(QWidget):
     def __init__(self, parent = None):
         super(ScopeCapture, self).__init__(parent)
         
-        rm = visa.ResourceManager('@py')
-        resources = rm.list_resources()
-        usb = list(filter(lambda x: 'USB' in x, resources))
-        device = rm.open_resource(usb[0])
-        device.timeout = None
-        self.scope = ds1000z.Ds1000z(device)
+        # rm = visa.ResourceManager('@py')
+        # resources = rm.list_resources()
+        # usb = list(filter(lambda x: 'USB' in x, resources))
+        # device = rm.open_resource(usb[0])
+        # device.timeout = None
+        # self.scope = ds1000z.Ds1000z(device)
 
         self.s3 = boto3.resource('s3')
         self.myBucket = self.s3.Bucket('relectrify-tools-oscilloscopedata')
@@ -46,6 +49,16 @@ class ScopeCapture(QWidget):
         self.pb = QPushButton('Capture')
         self.pb.clicked.connect(self.on_pb_clicked)
 
+        self.listOfButtons = []
+        self.pbChOne = self.setup_button("1")
+        self.pbChOne.clicked.connect(lambda: self.on_btn_clicked(self.pbChOne))
+        self.pbChTwo = self.setup_button("2")
+        self.pbChTwo.clicked.connect(lambda: self.on_btn_clicked(self.pbChTwo))
+        self.pbChThree = self.setup_button("3")
+        self.pbChThree.clicked.connect(lambda: self.on_btn_clicked(self.pbChThree))
+        self.pbChFour = self.setup_button("4")
+        self.pbChFour.clicked.connect(lambda: self.on_btn_clicked(self.pbChFour))
+
         logTextBox = QPlainTextEditLogger(self)
         logTextBox.setFormatter(logging.Formatter('%(asctime)s - %(levelname)s - %(message)s'))
         logging.getLogger().addHandler(logTextBox)
@@ -55,8 +68,15 @@ class ScopeCapture(QWidget):
         hbox.addWidget(self.cb)
         hbox.addWidget(self.pb)
 
+        channelHBox = QHBoxLayout()
+        channelHBox.addWidget(self.pbChOne)
+        channelHBox.addWidget(self.pbChTwo)
+        channelHBox.addWidget(self.pbChThree)
+        channelHBox.addWidget(self.pbChFour)
+
         vbox = QVBoxLayout()
         vbox.addLayout(hbox)
+        vbox.addLayout(channelHBox)
         vbox.addWidget(logTextBox.widget)
 
         self.setLayout(vbox)
@@ -67,9 +87,54 @@ class ScopeCapture(QWidget):
         centerPoint = QDesktopWidget().availableGeometry().center()
         qtRect.moveCenter(centerPoint)
         self.move(qtRect.topLeft())
+    
+    def setup_button(self, name):
+        self.btn = QPushButton(name)
+        self.btn.setCheckable(True)
+        return self.btn
+    
+    def on_btn_clicked(self, btn):
+        if btn in self.listOfButtons:
+            self.listOfButtons.remove(btn)
+            print("Removed button {}".format(btn.text()))
+        else:
+            self.listOfButtons.append(btn)
+            print("Added button {}".format(btn.text()))
+
+    def setup_data_file(self,filename):
+        with open(filename, 'w') as f:
+            f.write("Time\n")
+            for i in range(1,1201):
+                f.write(str(i)+"\n")
+
+    def append_csv(self,myfile,tmpfile,item,data):
+        with open(myfile, 'r') as csvinput:
+            with open(tmpfile, 'w') as csvoutput:
+                writer = csv.writer(csvoutput, lineterminator='\n')
+                reader = csv.reader(csvinput)
+                all = []
+                row = next(reader)
+                row.append("Ch {}".format(item.text()))
+                all.append(row)
+                for row in reader:
+                    # print(int(row[0]))
+                    row.append(data[int(row[0])-1])
+                    all.append(row)
+                writer.writerows(all)
 
     def on_pb_clicked(self):
+        myfile = 'test.csv'
+        tmpfile = 'temp.csv'
+        self.setup_data_file(myfile)
+        for item in self.listOfButtons:
+            # data = [randint(0,9) for x in range(1,1201)]
+            data = self.scope.get_data()
+            self.append_csv(myfile,tmpfile,item,data)
+            copyfile(tmpfile,myfile)
+        print("clicked pb")
+        return
         waveform = raw_data_to_string(self.scope.get_data())
+        print(waveform)
         filename = "_"+str(self.cb.currentText())+"_" + datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
         filename = self.createTempFile(filename)
         with open(filename+".csv", 'w') as f:
